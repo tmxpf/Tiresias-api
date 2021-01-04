2ac78c1f27bec033b9f782165bcad430f4bb574d
gyu <tmxpf@naver.com>
2021-01-04 21:07:31 +0900

    react view commit

---
+++ frontend/react-view/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('/api/now')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[message]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          {message}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
